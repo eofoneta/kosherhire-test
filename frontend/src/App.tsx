6a1d4a695df6b10e9c40278f7560aab271efda7b
Lordmaryo <emmanuelofoneta@gmail.com>
2025-07-11 08:29:40 +0100

    chore: add Docker support for backend and frontend with Nginx setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,10 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { TreeView } from "./components/treeView";
+// import { TreeView } from "./components/treeView";
 import { Toaster } from "sonner";
 import { DirectoryExplorer } from "./components/DirectoryExplorer";
 
 const App = () => {
   const queryClient = new QueryClient();
-
-  // TODO - MAKE A FOLDER/FILE ACTIVE WHEN YOU DOUBLE CLICK - {
-  // FIX - WHEN A CHILD NODE IS ACTIVE, DISPLAY PATHS TO THE ADDRESS BAR (INSTEAD OF REMOVING IT'S INHERITED FOLDERS)
-  // BUT HIGHLIGHT THE ACTIVE PATH IN THE ADDRESS AND THE FOLDER IT BELONGS
-  // }
-  // have the nodes be in the browser url
 
   return (
     <QueryClientProvider client={queryClient}>
